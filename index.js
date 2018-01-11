f91f21ea250cfd3da3553918bcb223ef93daad3d
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-01-11 20:21:49 +0000

    Error handling, bug fixes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const soap = require('soap');
 const express = require('express');
 const app = express();
-const { ACCESS_TOKEN } = process.env;
+const { version } = require('./package');
+const { ACCESS_TOKEN, PORT } = process.env;
 
 app.get('/', async (req, res) => {
-	if (!req.query.text) return res.status(400).json({ error: 'The parameter "text" is required.' });
 	if (!req.query.token) return res.status(400).json({ error: 'The parameter "token" is required.' });
 	if (req.query.token !== ACCESS_TOKEN) return res.status(401).json({ error: 'Invalid token.' });
-	const client = await soap.createClientAsync('https://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl');
-	const response = await client.yodaTalkAsync({ inputText: req.query.text });
-	return res.status(200).json({ response: response.return });
+	if (!req.query.text) return res.status(400).json({ error: 'The parameter "text" is required.' });
+	try {
+		const client = await soap.createClientAsync('https://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl');
+		const response = await client.yodaTalkAsync({ inputText: req.query.text });
+		return res.status(200).json({ version, response: response.return });
+	} catch (err) {
+		return res.status(500).json({ error: err.message });
+	}
 });
 
-app.listen(process.env.PORT);
+app.listen(PORT);
